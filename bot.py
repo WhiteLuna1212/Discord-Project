55f4382c692da9fea8b4da7ae8cd5adea6e3f074
WhiteLuna1212 <whitelunafoxstar@gmail.com>
2024-09-29 13:30:43 +0900

    Create bot.py
    봇 실행 코드 생성 및 간단한 명령어로 동작하는지 확인

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+TOKEN = 'MTI4OTgwNDc0MzIwNzU1NTE2Mg.GIQ8Zs.HIyj9iBBVg60ybb0xfEBgewuM5EW04w-oM6kcE'
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+# 아래는 테스트로 구축함
+# 봇이 준비되었을 때 실행되는 이벤트
+@bot.event
+async def on_ready():
+    print(f'봇이 로그인되었습니다. {bot.user.name}')
+
+# '/안녕' 명령어에 반응하는 기능
+@bot.command()
+async def 안녕(ctx):
+    await ctx.send(f'{ctx.author.mention} 안녕?')
+
+# 봇 실행
+bot.run(TOKEN)
